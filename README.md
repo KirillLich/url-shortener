a7ffb1b4388c244c68c30d16d86f49a2519cad87
Kirill_lovkov <lovkovk2@gmail.com>
2025-05-19 13:25:13 +0300

    added basic auth

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Copy of project from https://www.youtube.com/watch?v=rCJvW2xgnk0
 
 Developed with using Golang, Slog, SQLite and Chi
 
-Command to start the service
+Command to start the service: $env:CONFIG_PATH = "..\..\configs\local.yaml"; go run main.go
---
+++ internal/http-server/handlers/redirect/redirect.go
@@ -0,0 +1,61 @@
+package redirect
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@latest --name=URLGetter
+type URLGetter interface {
+	GetURL(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlGetter URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.redirect.new"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		//tightly coupled to router
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Error("alias is empty")
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, resp.Error("invalid request"))
+		}
+
+		resURL, err := urlGetter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to get url", sl.Err(err))
+
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("got url", slog.String("url", resURL))
+
+		http.Redirect(w, r, resURL, http.StatusFound)
+	}
+}
